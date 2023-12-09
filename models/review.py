c5346938b1fe9cbfa6e6e3930856067772248895
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-12-09 22:28:22 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python
-""" holds class Review"""
+#!/usr/bin/python3
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Representation of Review """
+    """ DOC DOC DOC """
     place_id = ""
     user_id = ""
     text = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+
+"""
+    def __init__(self, place_id, user_id):
+        Review.place_id = place_id
+        Review.user_id = user_id
+"""
